ba45ed3d2ef172576113a5ea2fb0b3981f8ab0cb
Angelo Cappelletti <angelocapp97@gmail.com>
2018-04-13 10:29:26 +0200

    Initial configuration

--- VidlyAuth/Models/IdentityModels.cs
+++ VidlyAuth/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace VidlyAuth.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("VidlyAuthDb", throwIfV1Schema: false)
         {
         }
 
